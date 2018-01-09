3ff28241349e42a24ba0f7f333d0581f3edf6ceb
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-01-09 11:38:35 +0100

    Add reconstruction to PCA

--- facial_recognition/model.py
+++ facial_recognition/model.py
@@ -11,12 +11,16 @@ class Projection:
     def project(self, X):
         """Project the new data using the fitted projection matrices."""
 
+    def reconstruct(self, X):
+        """Reconstruct the projected data back into the original space."""
+
 
 class PCA(Projection):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.X_mean = None
         self.U = None
+        self.P = None
 
     def fit(self, X, y=None):
         assert X.ndim == 2, 'X can only be a 2-d matrix'
@@ -40,6 +44,7 @@ class PCA(Projection):
             U = X.dot(U).dot(np.diag(1 / np.sqrt(S * (X.shape[0] - 1))))
 
         self.U = U
+        self.P = U[:, :self.n_components]
 
         return self
 
@@ -48,6 +53,7 @@ class PCA(Projection):
             'You must fit PCA before you can project'
 
         X -= self.X_mean
-        P = self.U[:, :self.n_components]
+        return np.dot(X, self.P)
 
-        return np.dot(X, P)
+    def reconstruct(self, X):
+        return np.dot(X, self.P.T) + self.X_mean
